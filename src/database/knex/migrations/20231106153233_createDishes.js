c3ab028f3b3b48560796f4dd0996bef9a1ff048f
axel <axelselbach90@gmail.com>
2023-11-06 13:10:12 -0300

    fix: added notNullable

--- src/database/knex/migrations/20231106153233_createDishes.js
+++ src/database/knex/migrations/20231106153233_createDishes.js
@@ -1,12 +1,12 @@
 exports.up = knex => knex.schema.createTable('dishes', table => {
   table.increments('id');
-  table.text('name');
-  table.text('description');
+  table.text('name').notNullable();
+  table.text('description').notNullable();
 
   table.string('picture').nullable();
   
-  table.integer('price');
-  table.text('category');
+  table.integer('price').notNullable();
+  table.text('category').notNullable();
 
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
