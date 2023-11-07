cc5e63bd9a9c9af641de8b7c2ca4d15c637182fc
axel <axelselbach90@gmail.com>
2023-11-07 17:35:54 -0300

    chore: added semicolon

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -8,27 +8,28 @@ class UsersController{
   async create(request, response){
     const {name, email, password} = request.body;
 
-    const database = await sqliteConnection()
+    const database = await sqliteConnection();
 
     const checkUserExist = await database.get(
       "SELECT * FROM users WHERE email = (?)",
       [email]
-    )
+    );
 
      if(checkUserExist){
       throw new AppError("This e-mail is already in use")
-    }
+    };
 
     const hashedPassword = await hash(password, 8)
 
     await database.run(
       "INSERT INTO users (name, email, password) VALUES(?,?,?)", 
       [name, email, hashedPassword]
-    )
+    );
 
 
     return response.status(201).json();
-  }
+    
+  };
 
   async update(request, response) {
     const {name, email, password, old_password} = request.body;
