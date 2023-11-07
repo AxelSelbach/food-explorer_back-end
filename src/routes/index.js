40ff991e5e426c8fbd4ebca37e73eeb199108275
axel <axelselbach90@gmail.com>
2023-11-07 19:18:05 -0300

    feat: created dish routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require('express');
 const usersRoutes = require('./users.routes');
 const sessionsRoutes = require('./sessions.routes');
 const ingredientsRoutes = require('./ingredients.routes')
+const dishesRoutes = require('./dishes.routes')
 
 const routes = Router();
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
 routes.use('/ingredients', ingredientsRoutes);
+routes.use('/dishes', dishesRoutes);
 
 module.exports = routes;
