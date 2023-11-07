6cde82b13120342b41ee7d7943973bd80ec50916
axel <axelselbach90@gmail.com>
2023-11-07 18:34:08 -0300

    feat: add create function to dishes controller

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -3,9 +3,37 @@ const AppError = require('../utils/AppError');
 
 class DishesController {
   async create(request, response) {
-    const user_id = request.user.id
-    const { name, description, picture, price, category } = request.body
+    const { name, description, picture, price, category, ingredients } = request.body;
+    const user_id = request.user.DishesController;
+
+    const checkDishExists = await knex("dishes")
+    .where("name", name)
+    .first();
     
+      if(checkDishExists){
+      throw new AppError("This dish has already been created", 409)
+    };
+
+    const { dish_id } = await knex("notes").insert({
+        name,
+        description,
+        picture,
+        price,
+        category,
+        ingredients,
+        user_id
+    });
+
+    const insertIngredients = ingredients.map((name) => {
+      return {
+        dish_id,
+        name
+      }
+    });
+
+    await knex("ingredients").insert(insertIngredients);
+
+    return response.status(201).json();
 
   }
 }
