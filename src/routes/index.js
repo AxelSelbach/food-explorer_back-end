cc6c910c9d98cd835224d84c43a7d8b78cdbd763
axel <axelselbach90@gmail.com>
2023-11-07 16:46:26 -0300

    fix: added ingredients routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require('express');
 
 const usersRoutes = require('./users.routes');
 const sessionsRoutes = require('./sessions.routes');
+const ingredientsRoutes = require('./ingredients.routes')
 
 const routes = Router();
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
+routes.use('/ingredients', ingredientsRoutes);
 
 module.exports = routes;
