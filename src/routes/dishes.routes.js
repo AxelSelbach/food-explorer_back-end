c8b04f722c68589851247d5efd4fe9c834ee3481
axel <axelselbach90@gmail.com>
2023-11-08 17:12:18 -0300

    feat: added all routes to methods

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -6,5 +6,8 @@ const dishesRoutes = Router();
 
 dishesRoutes.post('/', dishesController.create);
 dishesRoutes.put('/:id', dishesController.update);
+dishesRoutes.get('/:id', dishesController.show);
+dishesRoutes.get('/', dishesController.index);
+dishesRoutes.delete('/:id', dishesController.delete);
 
 module.exports = dishesRoutes;
