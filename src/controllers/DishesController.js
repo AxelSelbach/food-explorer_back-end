e1f25d1b707a8363628f7f07cd5778f73ce5c520
axel <axelselbach90@gmail.com>
2023-11-08 20:36:14 -0300

    fix: finished method index

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -1,114 +1,114 @@
-const knex = require('../database/knex');
-const AppError = require('../utils/AppError');
+const knex = require('../database/knex')
+const AppError = require('../utils/AppError')
 
 class DishesController {
   async create(request, response) {
     const { name, description, picture, price, category, ingredients } = request.body;
 
-    const checkDishExists = await knex("dishes")
-    .where("name", name)
-    .first();
-    
-      if(checkDishExists){
-      throw new AppError("This dish has already been created", 409)
-    };
+    const checkDishExists = await knex('dishes').where('name', name).first()
 
-    const [ dish_id ]  = await knex("dishes").insert({
-        name,
-        description,
-        picture,
-        price,
-        category
-    });
+    if (checkDishExists) {
+      throw new AppError('This dish has already been created', 409)
+    }
+
+    const [dish_id] = await knex('dishes').insert({
+      name,
+      description,
+      picture,
+      price,
+      category
+    })
 
     const insertIngredients = ingredients.map(name => {
       return {
         dish_id,
         name
       }
-    });
+    })
 
-    await knex("ingredients").insert(insertIngredients);
+    await knex('ingredients').insert(insertIngredients)
 
-    return response.json();
-
-  };
+    return response.json()
+  }
 
   async update(request, response) {
-    const dish_id = request.params.id;
-    const { name, description, price, category, ingredients } = request.body;
+    const dish_id = request.params.id
+    const { name, description, price, category, ingredients } = request.body
 
-    const updateDishes = await knex("dishes")
-    .where({ id: dish_id })
-    .update({
+    const updateDishes = await knex('dishes').where({ id: dish_id }).update({
       name,
       description,
       price,
-      category,
-    });
+      category
+    })
 
-    await knex("ingredients").where({ id: dish_id }).delete()
+    await knex('ingredients').where({ id: dish_id }).delete()
 
-    const updateIngredients = ingredients.map((name) => {
+    const updateIngredients = ingredients.map(name => {
       return {
         dish_id,
         name
       }
-    });
+    })
 
-    await knex("ingredients").where({ id: dish_id }).insert(updateIngredients)
-    
+    await knex('ingredients').where({ id: dish_id }).insert(updateIngredients)
 
-    return response.json(updateDishes);
-
-  };
+    return response.json(updateDishes)
+  }
 
   async show(request, response) {
-    const dish_id = request.params.id;
+    const dish_id = request.params.id
 
-    const dish = await knex("dishes")
-    .where({ id: dish_id })
-    .first();
+    const dish = await knex('dishes').where({ id: dish_id }).first()
 
-    if(!dish) {
-      throw new AppError("This dish does not exist", 404);
+    if (!dish) {
+      throw new AppError('This dish does not exist', 404)
     }
 
-    const ingredients = await knex("ingredients")
-    .where({ dish_id })
-    .orderBy("name");
+    const ingredients = await knex('ingredients')
+      .where({ dish_id })
+      .orderBy('name')
 
-    return response.json({ 
+    return response.json({
       ...dish,
       ingredients
-    });
-
-  };
+    })
+  }
 
   async index(request, response) {
-    const dish_id = request.params.id;
+    const { search } = request.query
 
-    const { name, ingredients } = request.query;
+    let dishSearch = knex('dishes')
+      .select([
+        'dishes.id',
+        'dishes.name',
+        'dishes.description',
+        'dishes.picture',
+        'dishes.price',
+        'dishes.category'
+      ])
+      .distinct()
+      .orderBy('dishes.name')
 
-    const dishes = knex("dishes")
-    .where("name", "like", `%${name}%`)
-    .where("ingredients", "like", `%${ingredients}%`)
-    .innerJoin("ingredients", "dish.id", "ingredients.dish_id")
-    .select("dishes.name", "dishes.description", "dishes.picture", "dishes.price", "dishes.category", "ingredients.ingredients");
+      if (search) {
+        dishSearch = dishSearch
+        .where('dishes.name', 'like', `%${search}%`)
+        .orWhere('ingredients.name', 'like', `%${search}%`)
+        .join('ingredients', 'dishes.id', '=', 'ingredients.dish_id')
+      }
 
-    return response.json(dishes);
+      const dishes = await dishSearch
 
-};
+    return response.json(dishes)
+  }
 
   async delete(request, response) {
-    const dish_id = request.params.id;
+    const dish_id = request.params.id
 
-    await knex("dishes").where({ id: dish_id }).delete();
+    await knex('dishes').where({ id: dish_id }).delete()
 
-    return response.json();
+    return response.json()
+  }
+}
 
-  };
-
-};
-
-module.exports = DishesController;
+module.exports = DishesController
