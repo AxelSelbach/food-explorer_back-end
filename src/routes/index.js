d62beb02d53cefc087cba984921333fe302bf6f9
axel <axelselbach90@gmail.com>
2023-12-26 18:09:11 -0300

    fix: added requests routes to index file

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const usersRoutes = require('./users.routes');
 const sessionsRoutes = require('./sessions.routes');
 
 const dishesRoutes = require('./dishes.routes');
+const requestsRoutes = require('./requests.routes');
 const ingredientsRoutes = require('./ingredients.routes');
 const favoritesRoutes = require('./favorites.routes');
 
@@ -11,6 +12,7 @@ const routes = Router();
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
 routes.use('/dishes', dishesRoutes);
+routes.use('/requests', requestsRoutes);
 routes.use('/ingredients', ingredientsRoutes);
 routes.use('/favorites', favoritesRoutes);
 
